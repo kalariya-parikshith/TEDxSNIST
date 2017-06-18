f7fb23a22f0d498190a8aa1882ab2c93a66e654f
Sai Lakshmi Yadlapati <sailakshmiyadlapati@gmail.com>
2017-06-18 18:45:18 +0530

    Added database.twig

--- includes/connection.php
+++ includes/connection.php
@@ -1,16 +1,29 @@
 <?php
-	global $con;
+
 	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-	$server = "localhost";
-	$user   = "root";
-	$pass   = "";
-	$db     = "tedxsnist";
+	$server = $url["host"];
+	$user   = $url["user"];
+	$pass   = $url["pass"];
+	$db     = substr($url["path"], 1);
 
-	/*$conn = new mysqli($server, $username, $password, $db);*/
+	/*$con = new mysqli($server, $user, $pass, $db);*/
 
 	$con=mysqli_connect("$server", "$user", "$pass", "$db") or die("Connection was not established");
 
+	$app->get('/db/', function() use($app) {
+  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+  $st->execute();
 
+  $names = array();
+  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+    $app['monolog']->addDebug('Row ' . $row['name']);
+    $names[] = $row;
+  }
+
+  return $app['twig']->render('database.twig', array(
+    'names' => $names
+  ));
+});
 
 ?>
